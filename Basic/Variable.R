20d3fb887ecf639bdd468f90255919a052313c34
Abhay1205Mishra <abhaym1205@gmail.com>
2024-02-02 12:10:26 +0530

    Learned basic of r programming

---
+++ Basic/Variable.R
@@ -0,0 +1,3 @@
+num_1<-5
+num_2<-10
+print(num_2)
---
+++ Basic/data_types.R
@@ -0,0 +1,13 @@
+# defining varible data types
+num_var<-10
+char_var<-'A'
+bool_var<-TRUE
+
+# getting the data types of variable
+class_num <- class(num_var)
+class_char<-class(char_var)
+class_bool<-class(bool_var)
+print(class_num )
+print(class_char )
+
+
