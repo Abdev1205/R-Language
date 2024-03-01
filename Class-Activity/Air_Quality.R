6e5c3880bbc87758f79143fba0eaea4d9d57221b
abhay <abhaym1205@gmail.com>
2024-03-01 09:07:43 +0530

    added air quality R code

--- Class-Activity/Air_Quality.R
+++ Class-Activity/Air_Quality.R
@@ -7,3 +7,7 @@ mean(airquality$Ozone, na.rm = TRUE)
 summary(airquality)
 boxplot(airquality)
 New_df <- airquality # nolint
+
+set.seed(123)
+data <- c(rnorm(100), 10, 12, -15, 20)
+boxplot(data, main = "Boxplot of Data with outliers")
